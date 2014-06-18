5be6056835676e2b75eb9a8b296441ad782f947c
vintagegamingsystems <vintagegamingsystems@gmail.com>
2014-06-18 14:26:02 -0700

    Create twinkle-twinkle.ps1

---
+++ twinkle-twinkle.ps1
@@ -0,0 +1,11 @@
+$noun1 = read-host "Noun"
+$noun2 = read-host "Noun"
+$noun3 = read-host "Noun"
+$adj1 = read-host "Adjective"
+
+write-host "Twinkle, twinkle, $adj1 $noun,"
+write-host "How I wonder what you are."
+write-host "Up above the $noun2 so high,"
+write-host "Like a $noun3 in the sky."
+write-host "Twinkle, twinkle, $adj1 $noun,"
+write-host "How I wonder what you are."
